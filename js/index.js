1fe01650f932fc1a7105c79797e159010985675b
Cristian Dinu <cristian.dinu@learnfwd.com>
2017-06-12 10:28:06 +0100

    First real commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,23 @@
 var App = require('lfa-core').App;
 
-App.book.on('render', function (opts) {
-  // Page has now changed to opts.chapter. Do something special
+App.book.on('render', function (/* opts */) {
+  var classroom = require('lfa-classroom');
+  var ClassmateList = classroom.ClassmateList;
+  var NameInput = classroom.NameInput;
+
+  var studentList = document.getElementById('student-list');
+  if (studentList) {
+    React.render(
+      React.createElement(ClassmateList, null),
+      studentList
+    );
+  }
+
+  var nameInput = document.getElementById('cover-name-input');
+  if (nameInput) {
+    React.render(
+      React.createElement(NameInput, null),
+      nameInput
+    );
+  }
 });
